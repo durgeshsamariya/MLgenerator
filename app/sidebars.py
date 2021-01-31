b2d192346e3a0fbb3bd6b0fca07228a0a75e63d2
durgeshsamariya <samariya.durgesh@gmail.com>
2021-01-31 11:52:26 +0530

    iForest sidebar added.

--- app/sidebars.py
+++ app/sidebars.py
@@ -53,6 +53,7 @@ def LOF_sidebar():
                 "Which one?", list(ANOMALY_DETECTION_DATASETS.keys())
             )
             inputs["dataset"] = ANOMALY_DETECTION_DATASETS[dataset]
+        st.write("## Model Hyperparameter")
         inputs['k'] = st.sidebar.number_input(
             "k?", 1, None, 10,
         )
@@ -67,6 +68,44 @@ def LOF_sidebar():
         )
         inputs["metric"] = DISTANCE_METRICS[dist]
 
+        inputs["visualization_status"] = st.checkbox('Visualization?', True)
+        if inputs["visualization_status"]:
+            inputs["save"] = st.checkbox('Do you want to save the figure?', False)
+    return inputs
+
+def iForest_sidebar():
+    inputs = {}
+    with st.sidebar:
+        st.write("## Input data")
+        inputs["data"] = st.selectbox(
+            "Which data set do you want to use?",
+            ("Synthetic data", "Benchmark data"),
+        )
+        if inputs["data"] == "Synthetic data":
+            inputs['contamination'] = st.sidebar.number_input(
+                "Contamination (percentage of outliers)", 0.0, None, 0.1, format="%f",
+            )
+            inputs['n_train'] = st.sidebar.number_input(
+                "number of training data points", 100, None, 200,
+            )
+            inputs['n_test'] = st.sidebar.number_input(
+                "number of testing data points", 50, None, 100,
+            )
+            inputs['n_features'] = st.sidebar.number_input(
+                "number of features in data set", 1, None, 2,
+            )
+        elif inputs["data"] == "Benchmark data":
+            dataset = st.selectbox(
+                "Which one?", list(ANOMALY_DETECTION_DATASETS.keys())
+            )
+            inputs["dataset"] = ANOMALY_DETECTION_DATASETS[dataset]
+        st.write("## Model Hyperparameter")
+        inputs['samples'] = st.sidebar.number_input(
+            "samples?", 2, None, 128,
+        )
+        inputs['trees'] = st.sidebar.number_input(
+            "trees?", 100, None, 100,
+        )
         inputs["visualization_status"] = st.checkbox('Visualization?', True)
         if inputs["visualization_status"]:
             inputs["save"] = st.checkbox('Do you want to save the figure?', False)
