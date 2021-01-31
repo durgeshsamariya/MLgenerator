b92bdb063c4606273558c441647468ba6a17455e
durgeshsamariya <samariya.durgesh@gmail.com>
2021-01-31 12:13:37 +0530

    update sidebars.py

--- app/sidebars.py
+++ app/sidebars.py
@@ -19,13 +19,51 @@ ANOMALY_DETECTION_DATASETS = {
     "Mnist": "mnist"
 }
 
-def kNN_sidebar():
+def kNN_AD_sidebar():
     inputs = {}
     with st.sidebar:
-        inputs['k'] = st.sidebar.number_input(
-            "k?", 1, None, 1000,
+        st.write("## Input data")
+        inputs["data"] = st.selectbox(
+            "Which data set do you want to use?",
+            ("Synthetic data", "Benchmark data"),
         )
-        
+        if inputs["data"] == "Synthetic data":
+            inputs['contamination'] = st.sidebar.number_input(
+                "Contamination (percentage of outliers)", 0.0, None, 0.1, format="%f",
+            )
+            inputs['n_train'] = st.sidebar.number_input(
+                "number of training data points", 100, None, 200,
+            )
+            inputs['n_test'] = st.sidebar.number_input(
+                "number of testing data points", 50, None, 100,
+            )
+            inputs['n_features'] = st.sidebar.number_input(
+                "number of features in data set", 1, None, 2,
+            )
+        elif inputs["data"] == "Benchmark data":
+            dataset = st.selectbox(
+                "Which one?", list(ANOMALY_DETECTION_DATASETS.keys())
+            )
+            inputs["dataset"] = ANOMALY_DETECTION_DATASETS[dataset]
+        st.write("## Model Hyperparameter")
+        inputs['k'] = st.sidebar.number_input(
+            "k?", 1, None, 10,
+        )
+        algo = st.selectbox(
+            "Which nearest neighbor search algorithm do you want to use?",
+            list(SEARCH_ALGORITHM.keys()),
+        )
+        inputs["search_algo"] = SEARCH_ALGORITHM[algo]
+        dist = st.selectbox(
+            "Which distance metric do you want to use?",
+            list(DISTANCE_METRICS.keys()),
+        )
+        inputs["metric"] = DISTANCE_METRICS[dist]
+
+        inputs["visualization_status"] = st.checkbox('Visualization?', True)
+        if inputs["visualization_status"]:
+            inputs["save"] = st.checkbox('Do you want to save the figure?', False)
+    return inputs
 
 def LOF_sidebar():
     inputs = {}
