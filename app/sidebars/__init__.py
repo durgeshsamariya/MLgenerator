c4c445f935c6aeda6a6e854892892b1737d1b43a
durgeshsamariya <samariya.durgesh@gmail.com>
2021-02-01 19:05:58 +0530

    Update __init__.py [Added kNN sidebar.]

--- app/sidebars/__init__.py
+++ app/sidebars/__init__.py
@@ -1,7 +1,9 @@
 from .anomaly_detection_sidebars import kNN_ad_sidebar
 from .anomaly_detection_sidebars import LOF_sidebar
 from .anomaly_detection_sidebars import iForest_sidebar
+from .classification_sidebars import kNN_sidebar
 
 __all__ = ['kNN_ad_sidebar',
             'LOF_sidebar',
-            'iForest_sidebar']
+            'iForest_sidebar',
+            'kNN_sidebar']
