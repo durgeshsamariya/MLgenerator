181b0d1cbb2cda3d831207180677639f829d81c4
durgeshsamariya <samariya.durgesh@gmail.com>
2021-02-01 20:32:47 +0530

    Classification sidebar Updated.

--- app/sidebars/classification_sidebars.py
+++ app/sidebars/classification_sidebars.py
@@ -24,6 +24,13 @@ KERNELS = {
     "Sigmoid": "sigmoid"
 }
 
+PENALTY = {
+    "L1": "l1",
+    "L2": "l2",
+    "Elastic Net": "elasticnet",
+    "None": "none"
+}
+
 def kNN_sidebar():
     inputs = {}
     with st.sidebar:
@@ -40,7 +47,7 @@ def kNN_sidebar():
                 "number of features in data set", 10, None, 10,
             )
             inputs['n_classes'] = st.sidebar.number_input(
-                "number of classes", 2, None, 3,
+                "number of classes", 2, None, 2,
             )
         elif inputs["data"] == "Benchmark data":
             dataset = st.selectbox(
@@ -81,7 +88,7 @@ def SVM_sidebar():
                 "number of features in data set", 10, None, 10,
             )
             inputs['n_classes'] = st.sidebar.number_input(
-                "number of classes", 2, None, 3,
+                "number of classes", 2, None, 2,
             )
         elif inputs["data"] == "Benchmark data":
             dataset = st.selectbox(
@@ -102,6 +109,41 @@ def SVM_sidebar():
             )
         if (kernel == "RBF" or kernel == "Polynomial" or kernel == "Sigmoid"):
             inputs["gamma"] = st.selectbox("Gamma (Kernel Coefficient)", ("scale", "auto"))
+        inputs["visualization_status"] = st.checkbox('Visualization?', False)
+        if inputs["visualization_status"]:
+            inputs['plots'] = st.multiselect("What metrics to plot?", ('ROC Curve', 'Precision-Recall Curve'))
+    return inputs
+
+def Logistic_Regression_sidebar():
+    inputs = {}
+    with st.sidebar:
+        st.write("## Input data")
+        inputs["data"] = st.selectbox(
+            "Which data set do you want to use?",
+            ("Synthetic data", "Benchmark data"),
+        )
+        if inputs["data"] == "Synthetic data":
+            inputs['n_samples'] = st.sidebar.number_input(
+                "number of data points", 100, None, 1000,
+            )
+            inputs['n_features'] = st.sidebar.number_input(
+                "number of features in data set", 10, None, 10,
+            )
+            inputs['n_classes'] = st.sidebar.number_input(
+                "number of classes", 2, None, 2,
+            )
+        elif inputs["data"] == "Benchmark data":
+            dataset = st.selectbox(
+                "Which one?", list(CLASSIFICATION_DATASETS.keys())
+            )
+            inputs["dataset"] = CLASSIFICATION_DATASETS[dataset]
+        
+        st.write("## Model Hyperparameter")
+        penalty = st.selectbox(
+            "Penalty", list(PENALTY.keys()))
+        inputs["penalty"] = PENALTY[penalty]
+        inputs['max_iter'] = st.number_input(
+            "Maximum number of iterations", 100, 1000, 100)
         inputs["visualization_status"] = st.checkbox('Visualization?', False)
         if inputs["visualization_status"]:
             inputs['plots'] = st.multiselect("What metrics to plot?", ('ROC Curve', 'Precision-Recall Curve'))
