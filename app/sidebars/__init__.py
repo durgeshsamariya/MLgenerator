994cd7cbd385b7eece2f2e7645e21ae426d8a8fb
durgeshsamariya <samariya.durgesh@gmail.com>
2021-02-01 19:59:08 +0530

    Update SVM sidebar.

--- app/sidebars/__init__.py
+++ app/sidebars/__init__.py
@@ -2,8 +2,10 @@ from .anomaly_detection_sidebars import kNN_ad_sidebar
 from .anomaly_detection_sidebars import LOF_sidebar
 from .anomaly_detection_sidebars import iForest_sidebar
 from .classification_sidebars import kNN_sidebar
+from .classification_sidebars import SVM_sidebar
 
 __all__ = ['kNN_ad_sidebar',
             'LOF_sidebar',
             'iForest_sidebar',
-            'kNN_sidebar']
+            'kNN_sidebar',
+            'SVM_sidebar']
