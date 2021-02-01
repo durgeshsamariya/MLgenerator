6541a8ef62296ac28e1e2ac02754d3a06315f1f5
durgeshsamariya <samariya.durgesh@gmail.com>
2021-02-01 20:50:35 +0530

    Update __init__.py

--- app/sidebars/__init__.py
+++ app/sidebars/__init__.py
@@ -4,10 +4,12 @@ from .anomaly_detection_sidebars import iForest_sidebar
 from .classification_sidebars import kNN_sidebar
 from .classification_sidebars import SVM_sidebar
 from .classification_sidebars import Logistic_Regression_sidebar
+from .classification_sidebars import RF_sidebar
 
 __all__ = ['kNN_ad_sidebar',
             'LOF_sidebar',
             'iForest_sidebar',
             'kNN_sidebar',
             'SVM_sidebar',
-            'Logistic_Regression_sidebar']
+            'Logistic_Regression_sidebar',
+            'RF_sidebar']
