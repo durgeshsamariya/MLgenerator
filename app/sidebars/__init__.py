86db6926bc6cb327336004dac2a2f8ecbb904bdf
durgeshsamariya <samariya.durgesh@gmail.com>
2021-02-01 20:32:15 +0530

    Logistic Regression sidebar added.

--- app/sidebars/__init__.py
+++ app/sidebars/__init__.py
@@ -3,9 +3,11 @@ from .anomaly_detection_sidebars import LOF_sidebar
 from .anomaly_detection_sidebars import iForest_sidebar
 from .classification_sidebars import kNN_sidebar
 from .classification_sidebars import SVM_sidebar
+from .classification_sidebars import Logistic_Regression_sidebar
 
 __all__ = ['kNN_ad_sidebar',
             'LOF_sidebar',
             'iForest_sidebar',
             'kNN_sidebar',
-            'SVM_sidebar']
+            'SVM_sidebar',
+            'Logistic_Regression_sidebar']
